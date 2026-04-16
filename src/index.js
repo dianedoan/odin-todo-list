0d7cb7e149b95f62576cd530c438637bbaca6f68
Diane Doan <dianeddoan@gmail.com>
2026-04-15 22:52:49 -0600

    Add App, Project, Todo objects with basic create functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "./styles.css";
+
+class App {
+    constructor() {
+        this.projectList = [];
+    }
+
+    createProject(title) {
+        const newProject = new Project(title);
+        this.projectList.push(newProject);
+        return newProject;
+    };
+}
+
+class Project {
+    constructor(title) {
+        this.title = title;
+        this.todoList = [];
+    }
+
+    createTodo(title, description, dueDate, priority) {
+        let newTodo = new Todo(title, description, dueDate, priority);
+        this.todoList.push(newTodo);
+    }
+}
+
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }   
+}
+
+const app = new App();
+const myProject1 = app.createProject("Project 1");
+const myProject2 = app.createProject("Project 2");
+const todo1 = myProject1.createTodo("finish project", "add styling", "04-14-2026", "high");
+const todo2 = myProject1.createTodo("finish chores", "wash dishes", "04-21-2026", "low");
+console.log(app);
